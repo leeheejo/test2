5237e5bf669b5c3168aa18a7fc70b722ed346b26
leeheejo <heejo1206@gmail.com>
2017-03-22 11:31:37 +0900

    add  comments

--- src/main/java/com/icontrols/test/HomeController.java
+++ src/main/java/com/icontrols/test/HomeController.java
@@ -43,7 +43,11 @@ public class HomeController {
 
 		return "home";
 	}
-
+	
+    /*
+     * move to join.jsp
+     * @return ModelAndView 
+     */
 	@RequestMapping("joinPage.do")
 	public ModelAndView joinPage() {
 		ModelAndView mav = new ModelAndView();
@@ -51,6 +55,13 @@ public class HomeController {
 		return mav;
 	}
 
+    /*
+     * insert DB and move to home.jsp
+     * @RequestParam uId (String) user's ID
+     * @RequestParamu Pwd (String) user's Password
+     * @RequestParam uPhone (String) user's PhoneNumber
+     * @return ModelAndView 
+     */
 	@RequestMapping("join.do")
 	public ModelAndView join(@RequestParam(value = "u_id") String uId, @RequestParam(value = "u_pwd") String uPwd,
 			@RequestParam(value = "u_phone") String uPhone) {
@@ -64,6 +75,10 @@ public class HomeController {
 		return mav;
 	}
 
+    /*
+     * move to login.jsp
+     * @return ModelAndView 
+     */
 	@RequestMapping("loginPage.do")
 	public ModelAndView loginPage() {
 		ModelAndView mav = new ModelAndView();
@@ -71,13 +86,25 @@ public class HomeController {
 		return mav;
 	}
 
+    /*
+     * selectDB for login and if(correct) move to success.jsp else move to fail.jsp
+     * @RequestParam uId (String) user's ID
+     * @RequestParamu Pwd (String) user's Password
+     * @return ModelAndView 
+     */
 	@RequestMapping("login.do")
 	public ModelAndView login(@RequestParam(value = "u_id") String uId, @RequestParam(value = "u_pwd") String uPwd) {
 		ModelAndView mav = new ModelAndView();
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("uId", uId);
 		map.put("uPwd", uPwd);
-		int loginCheck = UserService.loginCheck(map);
+		
+		/*
+		 * to check user's Id and user's pwd
+		 * @return success : return 1 / fail : return 0
+		 */
+		int loginCheck = UserService.loginCheck(map); 
+		
 		if (loginCheck == 1) {
 			mav.setViewName("success");
 		} else {
--- src/main/java/com/icontrols/test/UserDaoImpl.java
+++ src/main/java/com/icontrols/test/UserDaoImpl.java
@@ -11,18 +11,36 @@ public class UserDaoImpl implements UserDao {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
-	private final String PATH = "User.";
+	
+	//mapper's name 
+	private final String PATH = "User."; 
 
+	/*  call sqlSession
+	 *  @RequestParam map
+	 */
 	@Override
 	public void insertUser(HashMap<String, Object> map) {
 		// TODO Auto-generated method stub
+		
+		/*  access to the mapper
+		 *  @RequestParam statement mapper's name + statement's id
+		 *  @RequestParam parameter (for VALUES clause of the INSERT statement)
+		 */
 		sqlSession.insert(PATH + "insertUser", map);
 	}
 
+	
+	/*  call sqlSession
+	 *  @RequestParam map
+	 */
 	@Override
 	public int loginCheck(HashMap<String, Object> map) {
 		// TODO Auto-generated method stub
 
+		/*  access to the mapper
+		 *  @RequestParam statement statement mapper's name + statement's id
+		 *  @RequestParam parameter (for WHERE clause of the SELECT statement)
+		 */
 		return sqlSession.selectOne(PATH + "loginCheck", map);
 	}
 
